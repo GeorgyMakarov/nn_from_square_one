3e4bb2332a768a400229621680b685c54c2c178a
GeorgyMakarov <georgy.v.makarov@gmail.com>
2021-08-19 08:50:15 +0200

    add: planar classification nn

---
+++ planar_classifier.R
@@ -0,0 +1,5 @@
+# Implement binary classification neural network with a single hidden layer.
+# Evaluate classification performance using planar data and compare it to
+# logistic regression performance.
+
+
